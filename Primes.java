7e49022e70723a2274c0dc7459db2c3702526272
karavanii <elay.karavani@post.runi.ac.il>
2025-11-20 10:28:27 +0200

    3rdtry

--- Primes.java
+++ Primes.java
@@ -1,18 +1,28 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        double count =1;
+        int count = 0;
         System.out.println("Prime numbers up to " + n + ":");
-        System.out.println("2");
-        System.out.println("3");
-        count++;
-        for(int i = 2;i<n;i++){
-            if (i%3!=0 && i%2 != 0 && i%5 !=0) {
+        if (n >= 2) { 
+            System.out.println(2); count++; 
+        }
+        if (n >= 3) {
+             System.out.println(3); count++; 
+            }
+        for (int i = 4; i <= n; i++) {
+            boolean prime = true;
+            for (int d = 2; d * d <= i; d++) {
+                if (i % d == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
                 System.out.println(i);
                 count++;
             }
         }
-        int percent = (int)((count/ n)*100);
-        System.out.println("There are " +(int)count +" primes between 2 and "+n+" ("+(int)percent+"% are primes)");
+        int percent = (int)((count / (double)n) * 100);
+        System.out.println("There are " + count + " primes between 2 and " + n +" (" + percent + "% are primes)");
     }
 }
